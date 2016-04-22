6310647953b2838d5c04396ec027d73302b6a301
afelisatti <afelisatti@mulesoft.com>
2016-04-22 18:21:43 -0300

    MULE-9490: Migrate Http Connector to extensions API. First possible skeleton of the extension. Client provider code, but not linked to request operation.
    (cherry picked from commit 91237fda6d7567dc40c668777aa098d03bc52854)

---
+++ extensions/http/src/main/java/org/mule/extension/http/api/request/HttpRequesterOperations.java
@@ -0,0 +1,227 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.http.api.request;
+
+import org.mule.extension.http.api.HttpConnector;
+import org.mule.extension.http.api.HttpResponseAttributes;
+import org.mule.extension.http.api.HttpSendBodyMode;
+import org.mule.extension.http.api.HttpStreamingType;
+import org.mule.extension.http.api.request.authentication.HttpAuthentication;
+import org.mule.extension.http.api.request.authentication.UsernamePasswordAuthentication;
+import org.mule.extension.http.api.request.builder.HttpRequesterRequestBuilder;
+import org.mule.extension.http.api.request.validator.ResponseValidator;
+import org.mule.extension.http.api.request.validator.SuccessStatusCodeValidator;
+import org.mule.extension.http.internal.request.HttpRequestBuilder;
+import org.mule.extension.http.internal.request.HttpResponseToMuleMessage;
+import org.mule.extension.http.internal.request.MuleEventToHttpRequest;
+import org.mule.extension.http.internal.request.grizzly.GrizzlyHttpClient;
+import org.mule.runtime.api.temporary.MuleMessage;
+import org.mule.runtime.core.api.MessagingException;
+import org.mule.runtime.core.api.MuleEvent;
+import org.mule.runtime.core.api.MuleException;
+import org.mule.runtime.core.config.i18n.CoreMessages;
+import org.mule.runtime.core.util.StringUtils;
+import org.mule.runtime.extension.api.annotation.param.Connection;
+import org.mule.runtime.extension.api.annotation.param.Optional;
+import org.mule.runtime.extension.api.annotation.param.UseConfig;
+import org.mule.runtime.module.http.api.HttpConstants;
+import org.mule.runtime.module.http.internal.HttpParser;
+import org.mule.runtime.module.http.internal.domain.request.HttpRequest;
+import org.mule.runtime.module.http.internal.domain.request.HttpRequestAuthentication;
+import org.mule.runtime.module.http.internal.domain.response.HttpResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpRequesterOperations
+{
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequesterOperations.class);
+
+    static final String REMOTELY_CLOSED = "Remotely closed";
+
+    /**
+     * Consumes an HTTP service.
+     *
+     * @param path Path where the request will be sent.
+     * @param method The HTTP method for the request.
+     * @param host Host where the requests will be sent.
+     * @param port Port where the requests will be sent.
+     * @param source The expression used to obtain the body that will be sent in the request. Default is empty, so the payload will be used as the body.
+     * @param followRedirects Specifies whether to follow redirects or not.
+     * @param parseResponse Defines if the HTTP response should be parsed or it's raw contents should be propagated instead.
+     * @param requestStreamingMode Defines if the request should be sent using streaming or not.
+     * @param sendBodyMode Defines if the request should contain a body or not.
+     * @param responseTimeout Maximum time that the request element will block the execution of the flow waiting for the HTTP response.
+     * @param responseValidator Configures error handling of the response.
+     * @param config the {@link HttpConnector} configuration for this operation. All parameters not configured will be taken from it.
+     * @param muleEvent the current {@link MuleEvent}
+     * @return a {@link MuleMessage} with {@link HttpResponseAttributes}
+     */
+    public MuleMessage<Object, HttpResponseAttributes> request(String path,
+                                                          @Optional(defaultValue = "GET") String method,
+                                                          @Optional String host,
+                                                          @Optional Integer port,
+                                                          @Optional String source,
+                                                          @Optional Boolean followRedirects,
+                                                          @Optional Boolean parseResponse,
+                                                          @Optional HttpStreamingType requestStreamingMode,
+                                                          @Optional HttpSendBodyMode sendBodyMode,
+                                                          @Optional Integer responseTimeout,
+                                                          @Optional ResponseValidator responseValidator,
+                                                          @Optional HttpRequesterRequestBuilder requestBuilder,
+                                                          @Connection GrizzlyHttpClient client,
+                                                          @UseConfig HttpRequesterConfig config,
+                                                          MuleEvent muleEvent) throws MuleException
+    {
+        //TODO: Add request builder
+        if (host == null)
+        {
+            host = config.getHost().apply(muleEvent);
+        }
+
+        if (port == null)
+        {
+            port = config.getPort().apply(muleEvent);
+        }
+
+        if (followRedirects == null)
+        {
+            followRedirects = config.getFollowRedirects().apply(muleEvent);
+        }
+
+        if (parseResponse == null)
+        {
+            parseResponse = config.getParseResponse().apply(muleEvent);
+        }
+
+        if (requestStreamingMode == null)
+        {
+            requestStreamingMode = config.getRequestStreamingMode().apply(muleEvent);
+        }
+
+        if (sendBodyMode == null)
+        {
+            sendBodyMode = config.getSendBodyMode().apply(muleEvent);
+        }
+
+        if (responseTimeout == null)
+        {
+            responseTimeout = config.getResponseTimeout().apply(muleEvent);
+        }
+
+        if (responseValidator == null)
+        {
+            responseValidator = new SuccessStatusCodeValidator("0..399");
+        }
+
+        if (requestBuilder == null)
+        {
+            requestBuilder = new HttpRequesterRequestBuilder();
+        }
+
+        HttpAuthentication authentication = config.getAuthentication();
+        String resolvedPath = requestBuilder.replaceUriParams(buildPath(config.getBasePath().apply(muleEvent), path));
+        String uri = resolveUri(config.getScheme(), host, port, resolvedPath);
+        MuleEventToHttpRequest eventToHttpRequest = new MuleEventToHttpRequest(config, uri, method, requestStreamingMode, sendBodyMode, source);
+        HttpRequest httpRequest = createHttpRequest(muleEvent, eventToHttpRequest, requestBuilder,  authentication);
+
+        HttpResponse response;
+        try
+        {
+            //notificationHelper.fireNotification(this, muleEvent, httpRequest.getUri(), muleEvent.getFlowConstruct(), MESSAGE_REQUEST_BEGIN);
+            response = client.send(httpRequest, responseTimeout, followRedirects, resolveAuthentication(config.getAuthentication()));
+        }
+        catch (Exception e)
+        {
+            checkIfRemotelyClosed(e, config);
+            throw new MessagingException(CoreMessages.createStaticMessage("Error sending HTTP request"), muleEvent, e);
+        }
+
+        //TODO: modify to create a MuleMessage
+        HttpResponseToMuleMessage httpResponseToMuleMessage = new HttpResponseToMuleMessage(config, parseResponse);
+        MuleMessage<Object, HttpResponseAttributes> muleMessage = httpResponseToMuleMessage.convert(muleEvent, response, httpRequest.getUri());
+        //httpResponseToMuleEvent.convert(muleEvent, response, httpRequest.getUri());
+        //notificationHelper.fireNotification(this, muleEvent, httpRequest.getUri(), muleEvent.getFlowConstruct(), MESSAGE_REQUEST_END);
+
+        //TODO: Define way to perform the auth resend
+        //if (resendRequest(muleEvent, checkRetry, authentication))
+        //{
+        //    consumePayload(muleEvent);
+        //    muleEvent = innerProcess(muleEvent, false);
+        //}
+        //else
+        //{
+        //    validateResponse(muleEvent);
+        //}
+        //return muleEvent;
+
+        return muleMessage;
+    }
+
+    private String resolveUri(HttpConstants.Protocols scheme, String host, Integer port, String path)
+    {
+        // Encode spaces to generate a valid HTTP request.
+        String resolvedPath = HttpParser.encodeSpaces(path);
+
+        return String.format("%s://%s:%s%s", scheme, host, port, resolvedPath);
+    }
+
+    private HttpRequestAuthentication resolveAuthentication(HttpAuthentication authentication)
+    {
+        HttpRequestAuthentication requestAuthentication = null;
+        if (authentication instanceof UsernamePasswordAuthentication)
+        {
+            requestAuthentication = ((UsernamePasswordAuthentication) authentication).buildRequestAuthentication();
+        }
+        return requestAuthentication;
+    }
+
+    private HttpRequest createHttpRequest(MuleEvent muleEvent, MuleEventToHttpRequest muleEventToHttpRequest, HttpRequesterRequestBuilder requestBuilder, HttpAuthentication authentication) throws MuleException
+    {
+        HttpRequestBuilder builder = muleEventToHttpRequest.create(muleEvent, requestBuilder);
+
+        if (authentication != null)
+        {
+            authentication.authenticate(muleEvent, builder);
+        }
+        return builder.build();
+    }
+
+    private void checkIfRemotelyClosed(Exception exception, HttpRequesterConfig config)
+    {
+        if (config.getTlsContextFactory() != null && StringUtils.containsIgnoreCase(exception.getMessage(), REMOTELY_CLOSED))
+        {
+            logger.error("Remote host closed connection. Possible SSL/TLS handshake issue. Check protocols, cipher suites and certificate set up. Use -Djavax.net.debug=handshake for further debugging.");
+        }
+    }
+
+    protected String buildPath(String basePath, String path)
+    {
+        String resolvedBasePath = basePath;
+        String resolvedRequestPath = path;
+
+        if (!resolvedBasePath.startsWith("/"))
+        {
+            resolvedBasePath = "/" + resolvedBasePath;
+        }
+
+        if (resolvedBasePath.endsWith("/") && resolvedRequestPath.startsWith("/"))
+        {
+            resolvedBasePath = resolvedBasePath.substring(0, resolvedBasePath.length() - 1);
+        }
+
+        if (!resolvedBasePath.endsWith("/") && !resolvedRequestPath.startsWith("/") && !resolvedRequestPath.isEmpty())
+        {
+            resolvedBasePath += "/";
+        }
+
+
+        return resolvedBasePath + resolvedRequestPath;
+
+    }
+
+}
